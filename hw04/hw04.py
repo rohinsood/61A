441b4063573cc3e7a6111f0160715c880eefa421
rohinsood <rs.rohinsood@gmail.com>
2025-10-15 21:55:08 -0700

    hw04

--- hw04/hw04.py
+++ hw04/hw04.py
@@ -12,7 +12,14 @@ def shuffle(s):
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     """
     assert len(s) % 2 == 0, 'len(seq) must be even'
-    "*** YOUR CODE HERE ***"
+    print("DEBUG:", int(len(s)//2))
+    first_half = s[:(len(s)//2)]
+    second_half = s[(len(s)//2):]
+    interleaved = []
+    for i in range(len(first_half)):
+        interleaved.append(first_half[i])
+        interleaved.append(second_half[i])
+    return interleaved
 
 
 def deep_map(f, s):
@@ -37,7 +44,11 @@ def deep_map(f, s):
     >>> s3 is s2[1]
     True
     """
-    "*** YOUR CODE HERE ***"
+    for i in range(len(s)):
+        if type(s[i]) == list:
+            deep_map(f, s[i])
+        else:
+            s[i] = f(s[i])
 
 
 SOURCE_FILE = __file__
@@ -46,12 +57,12 @@ SOURCE_FILE = __file__
 def planet(mass):
     """Construct a planet of some mass."""
     assert mass > 0
-    "*** YOUR CODE HERE ***"
+    return ['planet', mass]
 
 def mass(p):
     """Select the mass of a planet."""
     assert is_planet(p), 'must call mass on a planet'
-    "*** YOUR CODE HERE ***"
+    return p[1]
 
 def is_planet(p):
     """Whether p is a planet."""
@@ -103,7 +114,13 @@ def balanced(m):
     >>> check(SOURCE_FILE, 'balanced', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    if is_planet(m):
+        return True
+    left_end = end(left(m))
+    right_end = end(right(m))
+    left_torque = length(left(m)) * total_mass(left_end)
+    right_torque = length(right(m)) * total_mass(right_end)
+    return (left_torque == right_torque) and balanced(left_end) and balanced(right_end)
 
 
 def berry_finder(t):
@@ -123,7 +140,13 @@ def berry_finder(t):
     >>> berry_finder(t)
     True
     """
-    "*** YOUR CODE HERE ***"
+    if t[0] == "berry":
+        return True
+    for b in branches(t):
+        if berry_finder(b):
+            return True
+    return False
+        
 
 
 SOURCE_FILE = __file__
@@ -138,7 +161,10 @@ def max_path_sum(t):
     >>> max_path_sum(t2) # 5, 2, 10
     17
     """
-    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return label(t)
+    else:
+        return label(t) + max([max_path_sum(b) for b in branches(t)])
 
 
 def mobile(left, right):
